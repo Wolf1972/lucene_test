d5d40e6ab8f5bf3c94c2fff76c40e59ec54975a7
Wolf1972 <new.volkov@gmail.com>
2023-04-23 00:44:42 +0300

    Batch indexing added

--- src/main/java/ru/bis/example/Main.java
+++ src/main/java/ru/bis/example/Main.java
@@ -1,5 +1,7 @@
 package ru.bis.example;
 
+import org.apache.lucene.index.IndexNotFoundException;
+import org.apache.lucene.index.IndexReader;
 import ru.bis.example.index.MessageIndexer;
 import ru.bis.example.index.MessageToDocument;
 import org.apache.lucene.document.Document;
@@ -9,14 +11,29 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
 
 public class Main {
+
+    final static private int BATCH_SIZE = 1000;
+    final static private String PATH_TO_INDEX = "/tmp/teaser_index";
 
     public static void main(String[] args) throws IOException, ParseException {
 
         System.out.println("Lucene example.");
 
-        final MessageIndexer indexer = new MessageIndexer("/tmp/teaser_index");
+        final MessageIndexer indexer = new MessageIndexer(PATH_TO_INDEX);
+        if (Files.exists(Paths.get(PATH_TO_INDEX))) {
+            try {
+                IndexReader indexReader = indexer.readIndex();
+                System.out.println("Index num docs: " + indexReader.numDocs());
+            }
+            catch (IndexNotFoundException e) {
+                System.out.println("Index directory is empty.");
+            }
+        }
 
         BufferedReader console = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Choose operation with index:");
@@ -26,7 +43,7 @@ public class Main {
         System.out.print("?: ");
         String operationStr = console.readLine();
 
-        long startTime = System. currentTimeMillis();
+        long startTime = 0;
 
         if (operationStr.equals("1") || operationStr.equals("2")) {
             String opStart;
@@ -45,14 +62,23 @@ public class Main {
             FileReader file = new FileReader(sourceFile);
             BufferedReader reader = new BufferedReader(file);
             String line = null;
+            startTime = System. currentTimeMillis();
             int lineCount = 0;
+            int batchCount = 0;
+            boolean firstBatch = true;
+            ArrayList<Document> list = new ArrayList<>();
             do {
                 line = reader.readLine();
-                if (line == null) break;
-                Document teaserDoc = MessageToDocument.createWith(Integer.toString(lineCount), line, "4081781010000" + String.format("%07d", lineCount));
-                indexer.index((lineCount == 0) && (operationStr.equals("2")), teaserDoc); // first iteration index created, then updated only
-                lineCount++;
-                if ((lineCount % 1000) == 0) System.out.println(lineCount + " lines indexed in " + ((System.currentTimeMillis() - startTime) / 1000) + " s.");
+                if (line == null || batchCount >= BATCH_SIZE) {
+                    indexer.index(firstBatch && (operationStr.equals("2")), list); // first iteration index created (if mode == create), then updated only
+                    firstBatch = false;
+                    list.clear();
+                    batchCount = 0;
+                    System.out.println(lineCount + " lines indexed in " + ((System.currentTimeMillis() - startTime) / 1000) + " s.");
+                    if (line == null) break;
+                }
+                list.add(MessageToDocument.createWith(Integer.toString(lineCount), line, "4081781010000" + String.format("%07d", lineCount)));
+                lineCount++; batchCount++;
             }
             while (true);
             System.out.println("Index " + opEnd + " for " + lineCount + " string(s) in " + ((System.currentTimeMillis() - startTime) / 1000) + " s.");
